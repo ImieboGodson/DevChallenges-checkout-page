0fd463c45ebb399f172509e027cbfcf0cf9c761f
Deifa <Deifa@users.noreply.github.com>
2021-01-10 10:22:56 +0100

    new fixes

--- resources/main.js
+++ resources/main.js
@@ -2,27 +2,34 @@ let discountedPrices = document.querySelectorAll('.d-price');
 let productQuantities = document.querySelectorAll('.qtn-value');
 const addButtons = document.querySelectorAll('.add-btn');
 const minusButtons = document.querySelectorAll('.minus-btn');
+let shippingFee = document.querySelector('.shipping').innerText = 19;
 let totalPrice = document.querySelector('.total').innerText = 148.98;
 
-
+//initiates the values of both discount prices when the page is loaded
 document.querySelector('.d-price[title=item1]').innerText = 54.99;
 document.querySelector('.d-price[title=item2]').innerText = 74.99;
 
+//storing the initial values each items discount price
 let initialPrice1 = 54.99;
 let initialPrice2 = 74.99;
 
+//initiates the value of the quantity of item in the cart when pthe page first loads
 productQuantities.forEach(pq => pq.innerText = 1);
 
-// function totalFunction () {
-//     let itemPrice1 = document.querySelector('.d-price[title=item1]').innerText;
-//     let itemPrice2 = document.querySelector('.d-price[title=item2]').innerText;
+
+//adds the each item's discount price and the shipping fee
+function totalFunction () {
+    let itemPrice1 = document.querySelector('.d-price[title=item1]').innerText;
+    let itemPrice2 = document.querySelector('.d-price[title=item2]').innerText;
     
-//     document.querySelector('.total').innerText = itemPrice1 + itemPrice2;
+    let total = document.querySelector('.total').innerText;
+    
+    total = parseFloat(itemPrice1) + parseFloat(itemPrice2) + parseFloat(shippingFee);
 
-//     console.log(itemPrice2);
-// }
+    document.querySelector('.total').innerText = total.toFixed(2);
+}
 
-
+//Runs everytime the add button is clicked
 function addFunction () {
     let title = this.getAttribute('title');
     let productQuantity = document.querySelector(`.qtn-value[title='${title}']`).innerText;
@@ -37,16 +44,16 @@ function addFunction () {
         discountedPrice = initialPrice1 * productQuantity
     } else if(title === 'item2') {
         discountedPrice = initialPrice2 * productQuantity
-    }
+    };
 
-    console.log(discountedPrice);
+    document.querySelector(`.d-price[title='${title}']`).innerText = discountedPrice.toFixed(2);
 
-    document.querySelector(`.d-price[title='${title}']`).innerText = discountedPrice.toFixed(2)
-
-    // totalFunction ();
+    totalFunction ();
     
 }
 
+
+//Runs everytime the minus button is clicked
 function minusFunction() {
     let title = this.getAttribute('title');
     let productQuantity = document.querySelector(`.qtn-value[title='${title}']`).innerText;
@@ -56,7 +63,7 @@ function minusFunction() {
         productQuantity -= 1;
     } else if (productQuantity === 1){
         productQuantity = 1;
-    }
+    };
 
 
     document.querySelector(`.qtn-value[title='${title}']`).innerText = productQuantity;
@@ -65,13 +72,14 @@ function minusFunction() {
         discountedPrice = initialPrice1 * productQuantity;
     } else if (title === 'item2') {
         discountedPrice = initialPrice2 * productQuantity;
-    }
+    };
 
     document.querySelector(`.d-price[title='${title}']`).innerText = discountedPrice.toFixed(2);
 
-    // totalFunction ();
+    totalFunction ();
 }
-
+//Calls the addFunction everytime the add button is clicked
 addButtons.forEach(addBtn => addBtn.addEventListener('click', addFunction));
 
+//Calls the minusFunction everytime the minus button is clicked
 minusButtons.forEach(minusBtn => minusBtn.addEventListener('click', minusFunction));
